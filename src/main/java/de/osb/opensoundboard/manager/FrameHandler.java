2599d1a00c1b3bc6591a66b2ee19ae4e16352b0f
keinesecrets1 <miethe.max@web.de>
2022-10-30 20:56:56 +0100

    Initial

--- src/main/java/de/osb/opensoundboard/manager/FrameHandler.java
+++ src/main/java/de/osb/opensoundboard/manager/FrameHandler.java
@@ -2,14 +2,24 @@ package de.osb.opensoundboard.manager;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 
-public class FrameHandler extends JFrame {
+public class FrameHandler extends JFrame implements ActionListener {
     public FrameHandler() throws HeadlessException {
         super("OSB | OpenSoundBoard v1.0 | by Max and Christian");
         this.setSize(750, 650);
         this.setResizable(false);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        Button one = new Button("Choose sound...");
+        one.setLocation(0, 0);
+        one.setActionCommand("chooser");
+        one.addActionListener(this);
+
+        this.add(one);
+
         this.show();
     }
 
@@ -26,5 +36,14 @@ public class FrameHandler extends JFrame {
     @Override
     protected void processWindowEvent(WindowEvent e) {
         super.processWindowEvent(e);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand().equalsIgnoreCase("chooser")) {
+            JFileChooser chooser = new JFileChooser();
+            chooser.setName("Choose sound...");
+            chooser.show();
+        }
     }
 }
